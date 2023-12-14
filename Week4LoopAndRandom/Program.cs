eb98b7d9be684beeb481a8fbe7a79daf0bc51562
Christian Haendel <example@example.com>
2023-12-14 20:53:32 +0200

    Week 4

---
+++ Week4LoopAndRandom/Program.cs
@@ -0,0 +1,14 @@
+﻿Random rnd = new Random();
+
+int myRandom;
+
+for (int i = 0; i < 3; i++)
+{
+    myRandom = rnd.Next(1, 11);
+    Console.WriteLine($"{i + 1}. {myRandom}");
+}
+
+//for (int i=0; i<10; i++)
+//{
+//    Console.WriteLine($"{i+1}. I will not skip Zoom classes!");
+//}
---
+++ Week4WhileLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿Random rnd = new Random();
+int cpuRandom = rnd.Next(1, 4);
+
+bool loopactive = true;
+int nmb_tries = 0;
+int userGuess=0;
+
+while (loopactive)
+{
+    nmb_tries++;
+    Console.WriteLine($"{nmb_tries}. try to guess the cpu number:");
+    userGuess=Convert.ToInt32(Console.ReadLine());
+ 
+    if (userGuess == cpuRandom)
+    {
+        Console.WriteLine($"You won! The cpu number was {cpuRandom}.");
+        loopactive = false;
+    }
+    else if (nmb_tries == 3)
+    {
+        Console.WriteLine("You loose!");
+        loopactive = false;
+    }
+}
+Console.WriteLine($"Number of tries: {nmb_tries}");
+Console.WriteLine("Have a nice day!");
